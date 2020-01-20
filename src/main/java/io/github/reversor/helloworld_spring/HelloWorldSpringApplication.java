68b8045b0b129a8d882c4028037a023df264b21f
Rost Zhiharev <revers914@gmail.com>
2020-01-20 15:15:17 +0400

    init

---
+++ src/main/java/io/github/reversor/helloworld_spring/HelloWorldSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.reversor.helloworld_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSpringApplication.class, args);
+    }
+
+}
